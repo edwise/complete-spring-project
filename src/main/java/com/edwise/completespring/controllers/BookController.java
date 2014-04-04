38f666356295bdf29037755699600838ae36e7c7
eanton <eduardo.anton@medianet.es>
2014-04-04 12:52:10 +0200

    Fixes de imports.

--- src/main/java/com/edwise/completespring/controllers/BookController.java
+++ src/main/java/com/edwise/completespring/controllers/BookController.java
@@ -7,7 +7,6 @@ import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
