4c5e29e43e2d515020f4da6d9269bd8ba2dd4c13
eanton <eduardo.anton@medianet.es>
2014-04-04 09:10:13 +0200

    Primer commit. Configuración básica de SpringBoot.

---
+++ src/main/java/com/edwise/completespring/Application.java
@@ -0,0 +1,18 @@
+package com.edwise.completespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ *
+ */
+@ComponentScan
+@EnableAutoConfiguration
+public class Application
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+}
