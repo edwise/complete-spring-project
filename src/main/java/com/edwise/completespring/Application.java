070de759ce023e043d4aec75e1d411ce69161f35
eanton <eduardo.anton@medianet.es>
2014-09-29 16:29:18 +0200

    Sonar fix, indentation.

--- src/main/java/com/edwise/completespring/Application.java
+++ src/main/java/com/edwise/completespring/Application.java
@@ -54,9 +54,8 @@ public class Application implements CommandLineRunner {
 
         // save a couple of books
         bookRepository.deleteAll();
-        bookRepository.save(new Book(BOOK_ID_1, "Libro prueba mongo", Arrays.asList(new Author().setName("Edu").setSurname
-                ("Antón")), "11-333-12",
-                new LocalDate(), new Publisher().setName("Editorial 1").setCountry("ES").setOnline(false)));
+        bookRepository.save(new Book(BOOK_ID_1, "Libro prueba mongo", Arrays.asList(new Author().setName("Edu").setSurname("Antón")),
+                "11-333-12", new LocalDate(), new Publisher().setName("Editorial 1").setCountry("ES").setOnline(false)));
         bookRepository.save(new Book(BOOK_ID_2, "Libro prueba mongo 2", Arrays.asList(new Author().setName("Otro").setSurname("Más"),
                 new Author().setName("S.").setSurname("King")), "12-1234-12", new LocalDate(), new Publisher().setName("Editorial 4")
                     .setCountry("UK").setOnline(true)));
