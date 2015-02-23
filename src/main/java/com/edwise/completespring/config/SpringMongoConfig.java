913ad49008dbfa7aec83dda8ccdc21a4d6fce013
eanton <eduardo.anton@medianet.es>
2015-02-23 17:56:59 +0100

    Added converters for Java8Date to MongoDB.

--- src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
+++ src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
@@ -4,9 +4,13 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
 
 import java.net.UnknownHostException;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
@@ -19,5 +23,26 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
     @Bean
     public Mongo mongo() throws UnknownHostException {
         return new MongoClient("localhost");
+    }
+
+    @Override
+    public CustomConversions customConversions() {
+        return new CustomConversions(Arrays.asList(
+                new LocalDateToStringConverter(),
+                new StringToLocalDateConverter()));
+    }
+
+    class LocalDateToStringConverter implements Converter<LocalDate, String> {
+        @Override
+        public String convert(LocalDate localDate) {
+            return localDate.toString();
+        }
+    }
+
+    class StringToLocalDateConverter implements Converter<String, LocalDate> {
+        @Override
+        public LocalDate convert(String source) {
+            return LocalDate.parse(source);
+        }
     }
 }
