628d024a0879b254e5e95a688eded76406197201
eanton <eduardo.anton@medianet.es>
2015-02-24 09:17:01 +0100

    Mongo DB config moved to properties.

--- src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
+++ src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
@@ -2,6 +2,7 @@ package com.edwise.completespring.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -14,22 +15,32 @@ import java.util.Arrays;
 
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
+
+    @Value("${db.name}")
+    private String databaseName;
+
+    @Value("${db.host}")
+    private String databaseHost;
+
     @Override
     public String getDatabaseName() {
-        return "springBootDB";
+        return databaseName;
     }
 
     @Override
     @Bean
     public Mongo mongo() throws UnknownHostException {
-        return new MongoClient("localhost");
+        return new MongoClient(databaseHost);
     }
 
     @Override
     public CustomConversions customConversions() {
-        return new CustomConversions(Arrays.asList(
-                new LocalDateToStringConverter(),
-                new StringToLocalDateConverter()));
+        return new CustomConversions(
+                Arrays.asList(
+                        new LocalDateToStringConverter(),
+                        new StringToLocalDateConverter()
+                )
+        );
     }
 
     class LocalDateToStringConverter implements Converter<LocalDate, String> {
