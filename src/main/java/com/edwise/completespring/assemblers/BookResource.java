4181ff1b4cd2572b52dd1e1e06fe7b29d18d03ee
eanton <eduardo.anton@medianet.es>
2015-05-11 17:14:39 +0200

    Sonar fixes...

--- src/main/java/com/edwise/completespring/assemblers/BookResource.java
+++ src/main/java/com/edwise/completespring/assemblers/BookResource.java
@@ -1,6 +1,7 @@
 package com.edwise.completespring.assemblers;
 
 import com.edwise.completespring.entities.Book;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -9,6 +10,7 @@ import org.springframework.hateoas.ResourceSupport;
 @Getter
 @Setter
 @Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
 public class BookResource extends ResourceSupport {
     private Book book;
 }
