1ce21b798ab03a5dd01cbe62a6b52c6135c76137
eanton <eduardo.anton@medianet.es>
2015-12-23 12:28:51 +0100

    Removed Spring Mongo DB configuration for JSR-310 conversion (new api date of Java 8), not needed with the new version of spring DATA Mongo DB.

--- src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
+++ src/main/java/com/edwise/completespring/config/SpringMongoConfig.java
@@ -5,13 +5,9 @@ import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.convert.CustomConversions;
 
 import java.net.UnknownHostException;
-import java.time.LocalDate;
-import java.util.Arrays;
 
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
@@ -33,27 +29,4 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
         return new MongoClient(databaseHost);
     }
 
-    @Override
-    public CustomConversions customConversions() {
-        return new CustomConversions(
-                Arrays.asList(
-                        new LocalDateToStringConverter(),
-                        new StringToLocalDateConverter()
-                )
-        );
-    }
-
-    class LocalDateToStringConverter implements Converter<LocalDate, String> {
-        @Override
-        public String convert(LocalDate localDate) {
-            return localDate.toString();
-        }
-    }
-
-    class StringToLocalDateConverter implements Converter<String, LocalDate> {
-        @Override
-        public LocalDate convert(String source) {
-            return LocalDate.parse(source);
-        }
-    }
 }
